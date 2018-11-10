8722006be20caadc30a73b73526b56155703afe8
stuckbroker <stuckbroker@gmail.com>
2018-11-10 18:57:10 -0500

    Spring Boot

---
+++ src/test/java/com/stuckbroker/springboot/SpringBootLearning/SpringBootLearningApplicationTests.java
@@ -0,0 +1,16 @@
+package com.stuckbroker.springboot.SpringBootLearning;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootLearningApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
